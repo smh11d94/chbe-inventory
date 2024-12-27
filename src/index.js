104e4849e1cb346f3efc339ba462d5eba1912032
smh11d94 <52341454+smh11d94@users.noreply.github.com>
2024-12-27 13:17:10 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
