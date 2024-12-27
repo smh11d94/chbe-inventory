5222cec813cf7a3ab31c891b7dc69d3e3317d64c
smh11d94 <52341454+smh11d94@users.noreply.github.com>
2024-12-26 21:32:34 -0800

    Create aws-exports.js

---
+++ src/aws-exports.js
@@ -0,0 +1,12 @@
+const awsmobile = {
+  "aws_project_region": "ca-central-1",
+  "aws_cloud_logic_custom": [
+    {
+      "name": "chbe-inventory-api",
+      "endpoint": "https://vr1uvkwi0b.execute-api.ca-central-1.amazonaws.com/dev",
+      "region": "ca-central-1"
+    }
+  ]
+};
+
+export default awsmobile;
